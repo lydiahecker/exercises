51887f101238a8865e8a2dacfe2df4b267e1625a
Lydia Hecker <lydia-hecker@student.kirkwood.edu>
2017-09-19 13:17:42 +0000

    Worked on practice exercise for python.

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,19 +6,19 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Blue Sargent")
 
 # display the address
-
+print("300 Fox Way")
 
 # display the city, state and zip
-
+print("Henrietta, Virginia, 26147")
 
 # display the phone number
-
+print("804-333-3333")
 
 # display the course area
-
+print("CIS-121")
 
 
 
