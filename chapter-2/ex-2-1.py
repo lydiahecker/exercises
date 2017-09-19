14857507209834b7b4501c0e06287eaeac9b90f8
Lydia Hecker <lydia-hecker@student.kirkwood.edu>
2017-09-19 13:42:23 +0000

    More python practice. Using variables.

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,20 +6,22 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-print("Blue Sargent")
+name = "Blue Sargent"
 
 # display the address
-print("300 Fox Way")
+address = "300 Fox Way"
 
 # display the city, state and zip
-print("Henrietta, Virginia, 26147")
+city_state_zip = "Henrietta, Virginia, 26147"
 
 # display the phone number
-print("804-333-3333")
+phone = "804-333-3333"
 
 # display the course area
-print("CIS-121")
+course = "CIS-121"
 
-
-
-
+print(name)
+print(address)
+print(city_state_zip)
+print(phone)
+print(course)
