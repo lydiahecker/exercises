e2ba2e01a6e2d53f54793c89b9d910d197362082
Lydia Hecker <lydia-hecker@student.kirkwood.edu>
2017-09-21 13:55:07 +0000

    Continued to work on learning python.

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -20,8 +20,11 @@ phone = "804-333-3333"
 # display the course area
 course = "CIS-121"
 
-print(name)
-print(address)
-print(city_state_zip)
-print(phone)
-print(course)
+def print_address():
+    print(name)
+    print(address)
+    print(city_state_zip)
+    print(phone)
+    print(course)
+
+print_address()
